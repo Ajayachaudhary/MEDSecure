20b0565fdeaabb8bbe6cf80032dbefe8d3481495
NirajChaudhary143 <nirajchy2000@gmail.com>
2024-12-22 22:28:48 +0545

    Added - individual chat template

--- Chat/views.py
+++ Chat/views.py
@@ -3,22 +3,7 @@ from django.contrib.auth.models import User
 from .models import Mesaage
 from django.db.models import Q
 
-def chat_with_user(request, username):
-    # Get the logged-in user
-    logged_user = request.user
-
-    # Get the user to chat with
-    other_user = get_object_or_404(User, username=username)
-
-    # Fetch messages between the logged-in user and the selected user
-    messages = Mesaage.objects.filter(
-        (Q(sender=logged_user) & Q(receiver=other_user)) |
-        (Q(sender=other_user) & Q(receiver=logged_user))
-    ).order_by('timestamp')
-
-    # Render the chat page with the fetched messages
-    return render(request, 'chat_with_user.html', {
-        'messages': messages,
-        'logged_user': logged_user,
-        'other_user': other_user,
-    })
+def chat(request, sender, receiver):
+	user_id = request.user
+	messages = Mesaage.objects.filter(Q(sender=user_id) | Q(receiver_id=user_id)).order_by('timestamp')
+	return render(request, "chat/chat.html", {'messages': messages, 'logged_user_name': sender, 'other_user_name': receiver})
