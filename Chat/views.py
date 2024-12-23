9866db40dac0c98bf09b67ee2f90097bba81f983
NirajChaudhary143 <nirajchy2000@gmail.com>
2024-12-23 21:35:42 +0545

    Fix - No message issue in chat template

--- Chat/views.py
+++ Chat/views.py
@@ -11,7 +11,7 @@ def chat(request, user, send_to):
 	messages = Mesaage.objects.filter(Q(sender=user_id) | Q(receiver_id=user_id)).order_by('timestamp')
 	other_user = messages.exclude(sender=user_id).first()
 	if other_user and other_user.sender.username != send_to:
-		return render(request, 'chat/chat.html', {'status': 'error', 'message': 'Opps Error!'})
+		return render(request, 'chat/chat.html', {'status': 'error', 'messages': 'No Messages...', 'other_user_name': send_to, 'doctors' : doctors, 'current_user': user})
 
 	return render(request, "chat/chat.html", {'messages': messages, 'current_user': user, 'other_user_name': send_to, 'doctors' : doctors})
 
