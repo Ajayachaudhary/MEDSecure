bbac029ec63ae7ec35ee801c9714520486c19949
Niraj Chaudhary <nirajchy2000@gmail.com>
2025-01-04 12:55:03 +0545

    Added - Image preview on load

--- Chat/models.py
+++ Chat/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 class Mesaage(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
-    content = models.TextField(null=True, blank=True) 
-    image = models.ImageField(upload_to='static/uploads/images/', null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='chat_images/', null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- MEDSecure/settings.py
+++ MEDSecure/settings.py
@@ -139,3 +139,6 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels.layers.InMemoryChannelLayer"
     }
 }
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
